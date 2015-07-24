2afdfc847456883ba8cca58bea090a96a2a1dd94
expll <tiny@TinydeMac-mini.local>
2015-07-24 17:53:13 +0800

    优化启动流程

--- Deamo.m
+++ Deamo.m
@@ -56,6 +56,11 @@ UIBackgroundTaskIdentifier bgTaskID;
     swizzledSelector = @selector(Deamo_applicationDidEnterBackground:);
     [self SwizzledMethod:class OrigSEL:originalSelector SwizzledSEL:swizzledSelector];
     
+    class = objc_getClass("AppDelegate");
+    originalSelector = @selector(applicationDidBecomeActive:);
+    swizzledSelector = @selector(Deamo_applicationDidBecomeActive:);
+    [self SwizzledMethod:class OrigSEL:originalSelector SwizzledSEL:swizzledSelector];
+    
     
 }
 
@@ -103,6 +108,79 @@ UIBackgroundTaskIdentifier bgTaskID;
     
     [self Deamo_applicationDidEnterBackground:application];
 }
+
+- (void)Deamo_applicationDidBecomeActive:(UIApplication *)application
+{
+    NSString *key = [NSString stringWithFormat:@"%@-launch", [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"]];
+    
+    NSString *value = [[self class] load:key];
+    NSLog(@"value: %@", value);
+    
+    if (value != nil) {
+        [[self class] delete:key];
+        
+        NSString *str = nil;
+        NSArray *arr = @[str];
+        NSLog(@"%@", arr);
+    }
+
+    
+    [self Deamo_applicationDidBecomeActive:application];
+}
+
+
++ (NSMutableDictionary *)getKeychainQuery:(NSString *)service {
+    return [NSMutableDictionary dictionaryWithObjectsAndKeys:
+            (__bridge id)kSecClassGenericPassword,(__bridge id)kSecClass,
+            service, (__bridge id)kSecAttrService,
+            service, (__bridge id)kSecAttrAccount,
+            (__bridge id)kSecAttrAccessibleAfterFirstUnlock,(__bridge id)kSecAttrAccessible,
+            nil];
+}
+
++ (void)save:(NSString *)service data:(id)data {
+    //Get search dictionary
+    NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
+    //Delete old item before add new item
+    SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
+    //Add new object to search dictionary(Attention:the data format)
+    [keychainQuery setObject:[NSKeyedArchiver archivedDataWithRootObject:data] forKey:(__bridge id)kSecValueData];
+    //Add item to keychain with the search dictionary
+    SecItemAdd((__bridge CFDictionaryRef)keychainQuery, NULL);
+}
+
++ (id)load:(NSString *)service {
+    id ret = nil;
+    NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
+    //Configure the search setting
+    //Since in our simple case we are expecting only a single attribute to be returned (the password) we can set the attribute kSecReturnData to kCFBooleanTrue
+    [keychainQuery setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    [keychainQuery setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    CFDataRef keyData = NULL;
+    if (SecItemCopyMatching((__bridge CFDictionaryRef)keychainQuery, (CFTypeRef *)&keyData) == noErr) {
+        @try {
+            ret = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge NSData *)keyData];
+        } @catch (NSException *e) {
+            NSLog(@"Unarchive of %@ failed: %@", service, e);
+        } @finally {
+        }
+    }
+    if (keyData)
+        CFRelease(keyData);
+    return ret;
+}
+
++ (void)delete:(NSString *)service {
+    NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
+    SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
+}
+
+
+
+
+
+
+
 
 
 
